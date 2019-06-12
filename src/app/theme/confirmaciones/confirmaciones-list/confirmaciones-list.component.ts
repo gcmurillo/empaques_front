ec42b35961948d8e87b2a933b7e86f989a27f6b7
gcmurillo <gcmurillo@outlook.com>
2019-06-12 15:02:38 -0500

    table confirmaciones

--- src/app/theme/confirmaciones/confirmaciones-list/confirmaciones-list.component.ts
+++ src/app/theme/confirmaciones/confirmaciones-list/confirmaciones-list.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { TransaccionesService } from '../../../services/transacciones/transacciones.service';
 
 @Component({
   selector: 'app-confirmaciones-list',
   templateUrl: './confirmaciones-list.component.html',
-  styleUrls: ['./confirmaciones-list.component.scss']
+  styleUrls: [
+    './confirmaciones-list.component.scss',
+    '../../../../assets/icon/icofont/css/icofont.scss'
+  ]
 })
 export class ConfirmacionesListComponent implements OnInit {
 
-  constructor() { }
+  ordenes = [];
+  rows = [];
+
+  columns: any[] = [
+    {
+      prop: 'orden.__str__',
+      name: 'Nombre Orden'
+    },
+    {
+      prop: 'orden.ubicacion_inicial.bodega.nombre',
+      name: 'Bodega'
+    },
+    {
+      prop: 'empaque.codigo',
+      name: 'Codigo Empaque'
+    },
+    {
+      prop: 'empaque.tipo_empaque.nombre',
+      name: 'Tipo empaque'
+    }
+  ]
+
+  constructor(private transaccionService: TransaccionesService,
+              ) { }
 
   ngOnInit() {
+    this.transaccionService.getOrdenesEmpaques().subscribe(
+      ordenes => { 
+        this.rows = ordenes;
+        console.log(ordenes);
+      },
+      err => console.log('error: ' + err.status)
+    );
   }
+
+  onActivate(event) {}
 
 }
--- src/app/theme/confirmaciones/confirmaciones.module.ts
+++ src/app/theme/confirmaciones/confirmaciones.module.ts
@@ -4,10 +4,23 @@ import { CommonModule } from '@angular/common';
 import { ConfirmacionesRoutingModule } from './confirmaciones-routing.module';
 import { ConfirmacionesListComponent } from './confirmaciones-list/confirmaciones-list.component';
 
+import {FormsModule, ReactiveFormsModule, FormGroup, Validators} from '@angular/forms';
+import {CustomValidators} from 'ng2-validation';
+import {DataTableModule} from 'angular2-datatable';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {ArchwizardModule} from 'ng2-archwizard/dist';
+import {SharedModule} from '../../shared/shared.module';
+
+
 @NgModule({
   imports: [
     CommonModule,
-    ConfirmacionesRoutingModule
+    ConfirmacionesRoutingModule,
+    NgxDatatableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ArchwizardModule,
+    SharedModule,
   ],
   declarations: [ConfirmacionesListComponent]
 })
